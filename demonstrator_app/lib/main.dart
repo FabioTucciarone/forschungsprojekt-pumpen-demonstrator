363c4d60a8cceccd8a9bca2967755f2b727aa43e
UKrause <ur.krause18@gmail.com>
2023-10-11 18:23:38 +0200

    Color Slider zum Teil erstellt

--- demonstrator_app/lib/main.dart
+++ demonstrator_app/lib/main.dart
@@ -42,7 +42,7 @@ class MainApp extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 Text('Durchlässigkeit'),
-                Text('Druck'),
+                PressureSlider(1000),
                 Text('Position der Wärmepumpe'),
               ],
             ),
@@ -142,6 +142,135 @@ class DrawerDemonstrator extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class PressureSlider extends StatefulWidget {
+  final double width;
+  const PressureSlider(this.width);
+
+  @override
+  State<PressureSlider> createState() => _PressureSliderState();
+}
+
+class _PressureSliderState extends State<PressureSlider> {
+  final List<Color> colorsGradient = [
+    const Color.fromARGB(255, 182, 2, 2),
+    const Color.fromARGB(255, 255, 0, 0),
+    const Color.fromARGB(255, 244, 153, 153),
+    const Color.fromARGB(255, 255, 255, 255),
+    const Color.fromARGB(255, 64, 141, 235),
+    const Color.fromARGB(255, 0, 115, 255),
+    const Color.fromARGB(255, 2, 69, 152),
+  ];
+  double currentValue = 870000;
+  double sliderPos = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    currentValue = determineValue(sliderPos);
+  }
+
+  double determineValue(double sliderPos) {
+    //...
+    return 0;
+  }
+
+  void correctingPosition(double position) {
+    if (position > widget.width) {
+      position = widget.width;
+    }
+    if (position < 0) {
+      position = 0;
+    }
+    setState(() {
+      sliderPos = position;
+      currentValue = determineValue(sliderPos);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        const Text(
+          'Druck: ',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        Center(
+          child: GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onHorizontalDragStart: (DragStartDetails details) {
+              correctingPosition(details.localPosition.dx);
+            },
+            onHorizontalDragUpdate: (DragUpdateDetails details) {
+              correctingPosition(details.localPosition.dx);
+            },
+            onTapDown: (TapDownDetails details) {
+              correctingPosition(details.localPosition.dx);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Container(
+                width: 800,
+                height: 50,
+                decoration: BoxDecoration(
+                  border: Border.all(width: 0.5, color: Colors.black),
+                  gradient: LinearGradient(colors: colorsGradient),
+                ),
+                child: CustomPaint(
+                  painter: SliderThumb(sliderPos),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+    /*return SliderTheme(
+        data: SliderTheme.of(context).copyWith(
+          inactiveTrackColor: Colors.white,
+          activeTrackColor: Colors.red,
+          thumbColor: Colors.black,
+          overlayColor: Colors.grey,
+          thumbShape: const RoundSliderThumbShape(
+            enabledThumbRadius: 15,
+            pressedElevation: 8,
+          ),
+          overlayShape: const RoundSliderOverlayShape(overlayRadius: 30),
+          trackShape: const RectangularSliderTrackShape(),
+          trackHeight: 10,
+        ),
+        child: Slider(
+          value: currentValue,
+          min: 870000,
+          max: 910000,
+          divisions: 1000,
+          label: '${currentValue.round()}',
+          onChanged: (double value) {
+            setState(() {
+              currentValue = value;
+            });
+          },
+        ));*/
+  }
+}
+
+class SliderThumb extends CustomPainter {
+  final double pos;
+  SliderThumb(this.pos);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawLine(Offset(pos, 0), Offset(pos, size.height),
+        Paint()..color = Colors.black);
+  }
+
+  @override
+  bool shouldRepaint(SliderThumb oldThumb) {
+    return true;
   }
 }
 
