cd50e942bcc8b1f80ff78695c461aa01f3c2cda1
st177288 <st177288@stud.uni-stuttgart.de>
2023-10-11 15:51:02 +0200

    egal

--- demonstrator_backend/demonstrator_backend.py
+++ demonstrator_backend/demonstrator_backend.py
@@ -1,3 +1,4 @@
+"""
 from flask import Flask, request, jsonify, redirect, url_for
 
 app = Flask(__name__)
@@ -11,5 +12,28 @@ def add():
     data = request.json
     print(data.get('permeability'))
     print(data.get('density'))
-    return str(data.get('permeability') + data.get('density'))
+    #return str(data.get('permeability') + data.get('density'))
+"""
 
+from flask import Flask, request, jsonify, redirect, url_for, Response
+import io
+import random
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+app = Flask(__name__)
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
+    
