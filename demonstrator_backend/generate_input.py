e86fb77e1d2f856f099e06e3647d27470cadffa5
Fabio Tucciarone <st177167@stud.uni-stuttgart.de>
2023-10-21 13:44:35 +0200

    Pipeline in lauffähigem stand, zu tun: Benutzerschnittstelle fertig machen

--- demonstrator_backend/generate_input.py
+++ demonstrator_backend/generate_input.py
@@ -12,15 +12,16 @@ from torch import load
 # Achtung, bisher alles noch Tests, die höchstwahrscheinlich nur bei mir lokal funktionieren
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "1HP_NN"))
 
-from main import run_from_demonstrator 
-from main import init_data 
+from main import demonstrator_run_1st_stage 
 
 # Achtung paths.yaml muss jetzt in diesem Ordner sein
 def test():
     "Aufrufen des manuell erstellten ein-Datenpunkt-Datensatzes im .h5-Format"
     path_to_data = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "data")
     path_to_1hp_model = os.path.join(path_to_data, "models_1hpnn","gksi1000","current_unet_dataset_2d_small_1000dp_gksi_v7")
-    results = run_from_demonstrator("dataset_raw_demonstrator_input_1dp", path_to_1hp_model, inputs="gksi")
+
+    results = demonstrator_run_1st_stage("dataset_raw_demonstrator_input_1dp", path_to_1hp_model, -1.0e-10, 2.0e-3)
+
     predicted_temperature = results[0]
     groundtruth_temperature = results[1]
     error_temperature = results[2]
