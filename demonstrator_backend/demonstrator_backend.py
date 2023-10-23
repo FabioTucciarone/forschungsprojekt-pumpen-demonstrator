08ae9ddba84197b8830f3a8f6ccfef48108f7bdd
st177288 <st177288@stud.uni-stuttgart.de>
2023-10-23 08:56:13 +0200

    imports

--- demonstrator_backend/demonstrator_backend.py
+++ demonstrator_backend/demonstrator_backend.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, jsonify, redirect, url_for, Response
 import io
 import os
+import sys
 import random
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
-from groundtruth import DataSet
+from dataclasses import dataclass
 import yaml
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "1HP_NN"))
-import preprocessing.prepare_1ststage as prep
+import preprocessing.prepare_1ststage as prepare
 
 app = Flask(__name__)
 
@@ -105,7 +106,7 @@ def get_settings(dataset: str = "dataset_raw_demonstrator_input_1dp"):
         paths = yaml.safe_load(f)
     default_raw_dir = paths["default_raw_dir"]
     path_to_settings = os.path.join(default_raw_dir, dataset, "inputs")
-    settings = get_pflotran_settings(path_to_settings)
+    settings = prepare.get_pflotran_settings(path_to_settings)
 
 # Debug run
 if __name__ == '__main__':
