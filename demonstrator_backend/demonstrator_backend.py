e29bb421caa0fcefe0ddbe7da0c89892a54a5aa4
st177821 <st177821@stud.uni-stuttgart.de>
2024-02-01 15:47:44 +0100

    farben

--- demonstrator_backend/demonstrator_backend.py
+++ demonstrator_backend/demonstrator_backend.py
@@ -232,7 +232,7 @@ def initialize_backend():
     # TODO: Hier einfach das einstellen, was hübsch aussieht!
     # Farbtupel: (R, G, B) mit 0 <= R, G, B <= 1
     color_palette = mc.ColorPalette(
-        cmap_list        = [(0.019,0.188,0.38), (1,1,1), (0.71,0.12,0.18)],
+        cmap_list        = [(0.019,0.188,0.38), (1,1,1), (0.71,0.3,0.3)],
         background_color = (1,1,1),
         text_color       = (0,0,0) 
     )
