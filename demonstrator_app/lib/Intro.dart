3b2269d465bb8f05f9e01093c0dd2308f18ea543
st177821 <st177821@stud.uni-stuttgart.de>
2024-02-02 14:31:52 +0100

    quick fix

--- demonstrator_app/lib/Intro.dart
+++ demonstrator_app/lib/Intro.dart
@@ -135,7 +135,7 @@ class _RobotIntroState extends State<RobotIntro> {
     "Eine Wärmefahne ist sozusagen das Feld, in dem sich die Wärme um die Pumpe herum ausbreitet. Schau mal, da unten kannst du sehen wie so etwas aussieht",
     "Ihr könnt euch das wie eine Fahne im Wind vorstellen, genauso folgt die Wärmefahne der Richtung des Grundwassers unterirdisch, verstehst du?",
     "Leider bin ich noch jung und tollpatschig. Kannst du mir helfen mich zu verbessern?",
-    "Du wirst gleich durch Schieberegler Eingaben machen können, dadurch berechne ich dann die Wärmefahnen. Je stärker mein Ergebnis von der Realität abweicht, desto höher wird deine Punktzahl sein. Schau mal, ich zeig dir was ich meine!",
+    "Du wirst gleich durch Schieberegler Eingaben machen können, dadurch berechne ich dann die Wärmefahnen. Je stärker mein Ergebnis von der Realität abweicht, desto höher wird deine Punktzahl sein. Schau mal, hier unten ist so ein Schieberegler, probier ihn mal aus!",
     "Hier siehst du, dass ich sehr schlecht war :( meine Wärmefahne (die obere) ist länger als sie sein soll. In der Mitte sieht man wie es eigentlich sein sollte und darunter den Unterschied. Das heißt für dich aber eine hohe Punktzahl, da ich jetzt weiß, was ich noch besser machen muss :)",
     "Dir wird automatisch ein Nutzername gegeben, damit man dich auf der Bestenliste verewigen kann. Dein Name wird oben rechts angezeigt. Viel Erfolg!",
     ""
@@ -204,13 +204,18 @@ class _RobotIntroState extends State<RobotIntro> {
       );
     } else if (times == 6) {
       return Positioned(
-          top: 300,
-          child: SizedBox(
-              width: 800,
-              child: Image.asset(
-                "assets/examplePlume.jpeg",
-                fit: BoxFit.contain,
-              )));
+        top: 300,
+        child: PressureSlider(
+          600,
+          const {
+            "pressure_range": [0.0, 100.0],
+            "permeability_range": [0.0, 100.0],
+          },
+          SliderType.dummy,
+          true,
+          true,
+        ),
+      );
     }
     return child;
   }
@@ -225,6 +230,7 @@ class _RobotIntroState extends State<RobotIntro> {
         child: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Center(
                   child: SizedBox(
@@ -292,20 +298,12 @@ class _RobotIntroState extends State<RobotIntro> {
                   ],
                 ),
               )),
-              PressureSlider(
-                600,
-                const {
-                  "pressure_range": [0.0, 100.0],
-                  "permeability_range": [0.0, 100.0],
-                },
-                SliderType.dummy,
-                true,
-                true,
-              ),
               const SizedBox(
                 height: 20,
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Center(
                     child: ElevatedButton(
