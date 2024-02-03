8a6bb21cdb8537346241032b739daf58183d9b9d
FabioTucciarone <fabiotucciarone@gmail.com>
2024-02-03 17:40:33 +0100

    Boxplot Code

--- demonstrator_backend/generate_boxplots.py
+++ demonstrator_backend/generate_boxplots.py
@@ -2,7 +2,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-def generate_boxplot(csv_file_name):
+show_title = True
+
+def generate_boxplot(csv_file_name, title):
     data_csv = pd.read_csv(csv_file_name)
     average_error_dp = np.array(data_csv['average_error'])
     min_error_dp = np.array(data_csv['min_error'])
@@ -10,9 +12,25 @@ def generate_boxplot(csv_file_name):
     time_dp = np.array(data_csv['time'])
 
     plt.figure(figsize=(10, 3))
+    if show_title: plt.title(title)
     plt.boxplot([average_error_dp, min_error_dp, max_error_dp], vert = False, widths=[0.6, 0.6, 0.6])
     plt.subplots_adjust(top=0.95, bottom=0.2)
-    plt.gca().set_yticklabels(['Rvaluereferenz', 'NRVO', 'kein NRVO'])
-    plt.xlabel("Zeit in µs")
+    plt.gca().set_yticklabels(['Mittlerer Fehler', 'Min. Fehler', 'Max. Fehler'])
+    plt.xlabel("Fehler in °C")
     plt.show()
 
+    plt.figure(figsize=(10, 3))
+    if show_title: plt.title(title)
+    plt.boxplot(time_dp, vert = False)
+    plt.subplots_adjust(top=0.95, bottom=0.2)
+    plt.xlabel("Zeit in s")
+    plt.show()
+
+def main():
+    generate_boxplot("interp_heuristic.csv", "Heuristik+Taylor")
+    generate_boxplot("interp_min.csv", "Min+Taylor")
+    generate_boxplot("interp_triangle_old.csv", "Quadranten+Taylor")
+    generate_boxplot("closest.csv", "Nächster")
+
+if __name__ == "__main__":
+    main()
