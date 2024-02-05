0ec008cc50d4d5d61e81ebfac7a2ffde9c0b28a2
FabioTucciarone <fabiotucciarone@gmail.com>
2024-02-05 22:17:39 +0100

    Hübscher

--- demonstrator_backend/generate_boxplots.py
+++ demonstrator_backend/generate_boxplots.py
@@ -9,22 +9,32 @@ def generate_boxplot(show_title=True):
     interp_heuristic_csv = pd.read_csv("measurements/performance_interp_seq_heuristic.csv")
     interp_old_triangle_csv = pd.read_csv("measurements/performance_interp_quad_heuristic.csv")
 
+    method_names = ["Nächster", "Minimum", "Seq. Heuristik", "Quadr. Heuristik"]
     fields = ["average_error", "max_error", "time"]
-    tick_distances = [0.01, 0.1, 0.1]
+    tick_distances = [0.01, 0.1, 0.05]
 
     for i, field in enumerate(fields):
         plt.figure(figsize=(10, 3))
         plt.grid(color='lightgray', linestyle='-', linewidth=0.5)
 
-        max_value = np.max(closest_csv)
+        data_frames = [closest_csv[field], interp_min_csv[field], interp_heuristic_csv[field], interp_old_triangle_csv[field]]
+        max_value = 0
+        for data_frame in data_frames:
+            max_value = max(max_value, np.max(data_frame))
+
         x_ticks = list(np.arange(0, max_value, tick_distances[i]))
         plt.gca().set_xticks(x_ticks)
 
-        boxplot = plt.boxplot([closest_csv[field], interp_min_csv[field], interp_heuristic_csv[field], interp_old_triangle_csv[field]], vert = False, showfliers=False)
-        
+        boxplot = plt.boxplot(data_frames, vert = False, showfliers=False)
+
+        for median_line in boxplot['medians']:
+            y = median_line.get_ydata()[0]
+            x = median_line.get_xdata()[0]
+            plt.text(x - max_value / 300, y - 0.2, np.round(x, decimals=5), size="small")
+                
         if show_title: plt.title(field)
         plt.subplots_adjust(top=0.95, bottom=0.2)
-        plt.gca().set_yticklabels(["Nächster", "Minimum", "Seq. Heuristik", "Quadr. Heuristik"])
+        plt.gca().set_yticklabels(method_names)
         if field == "average_error" or field == "max_error":
             plt.xlabel("Fehler in °C")
         else:
